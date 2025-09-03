2debc9d6ee3c66b95c5124f8aaf62e0c6278a405
Kritika Chakraborty <kritika7.kc@gmail.com>
2025-09-03 22:12:14 +0530

    Create StarPattern.java

---
+++ Patterns/StarPattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class StarPattern
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter number of rows:");
+      int n=sc.nextInt();
+      //Outer loop for rows
+      for(int i=1;i<=n;i++)
+        {
+          //Inner loop for printing stars
+          for(int j=1;j<=i;j++)
+            {
+              System.out.print("*");
+            }
+          System.out.println(); //move to nextline
+        }
+    }
+  }
