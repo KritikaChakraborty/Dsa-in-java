ab728d3ae98d100d93dd8f4d8de8314fddef50ed
Kritika Chakraborty <kritika7.kc@gmail.com>
2025-09-12 22:01:46 +0530

    Create HollowRectangle.java

---
+++ Patterns/HollowRectangle.java
@@ -0,0 +1,27 @@
+public class HollowRectangle {
+    public static void hollow_rectangle(int rows,int cols){
+        //outer loop
+        for(int i=1;i<=rows;i++)
+        {
+            //inner columns
+            for(int j=1;j<=cols;j++)
+            {
+                //cell-(i,j)
+                if(i==1||i==rows||j==1||j==cols)
+                {
+                    //boundary cells
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[])
+    {
+        hollow_rectangle(4,5);
+    }
+    }
+    
